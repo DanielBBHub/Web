b14d462c2f3d8f9ae97a8f09683a7ad1370cfb1d
DanielBBHub <63246222+DanielBBHub@users.noreply.github.com>
2022-10-14 09:25:24 +0200

    Update main_servidor_rest.js

--- ServidorREST/main_servidor_rest.js
+++ ServidorREST/main_servidor_rest.js
@@ -3,7 +3,7 @@
 // .....................................................................
 const express = require( 'express' )
 const bodyParser = require( 'body-parser' )
-const Logica = require( "../logica/Logica.js" )
+const Logica = require( "../Logica_negocio/Logica.js" )
 const cors = require('cors');
 
 
